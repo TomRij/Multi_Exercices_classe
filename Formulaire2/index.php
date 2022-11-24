a18511845bf160f064ede5c0f794626266afeb72
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2022-11-24 14:03:59 +0100

    fin des fonctions et fin du formulaire 2

--- Formulaire2/index.php
+++ Formulaire2/index.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire2</title>
+    <title>Tom Rijckaert</title>
 </head>
 <body>
-    <h1>Un super formulaire</h1>
-    <form action="" method=""></form>
+    <h1>Le Plus beau des formulaires</h1>
+    <form action="" method="">
         <fieldset>
             <legend>Données personnlles</legend>
             <div>
-                <label for="url">Votre prénom</label>
-                <input placeholder="prenom" type="text" id="prenom">
+                <label for="prenom">Votre prénom</label>
+                <input type="text" id="prenom">
             </div>
             <div>
                 <label for="nom">Votre nom</label>
-                <input placeholder="nom" type="text" id="nom">
+                <input type="text" id="nom">
             </div>
             <div>
                 <label for="date">Votre date de naissance</label>
@@ -28,17 +28,53 @@
             <legend>Les nouveaux inuput</legend>
             <div>
                 <label for="url">Votre URL</label>
-                <input placeholder="url" type="text" id="url">
+                <input type="text" id="url">
             </div>
             <div>
                 <label  for="telephone">Votre numéro de téléphone</label>
-                <input placeholder="telephone" type="text" id="telephone">
+                <input type="tel" id="telephone">
             </div>
             <div>
-                <label placeholder="telephone" for="telephone">Votre appréciation (entre 10 et20)</label>
+                <label for="appreciation">Votre appréciation (entre 10 et 20)</label>
                 <input type="range" id="appreciation">
             </div>
-            
+            <div>
+                <label for="couleur">Votre couleur préférée</label>
+                <input type="color" id="couleur">
+            </div>
+            <div>
+                <label for="recherche">Votre recherche</label>
+                <input type="search" id="recherche">
+            </div>
+            <div>
+                <label for="fichier">Choisir un fichier</label>
+                <input type="file" id="fichier">
+            </div>
+            <div>
+                <label for="heure">Choisir une heure</label>
+                <input type="time" id="heure">
+            </div>
+            <div>
+                <label for="mois">Choisir un mois</label>
+                <input type="month" id="mois">
+            </div>
+            <div>
+                <label for="semaine">Choisir une semaine</label>
+                <input type="week" id="semaine">
+            </div>
+            <div>
+                <label for="explications">Vos explications</label>
+                <textarea rows="5" id="explications"></textarea>
+            </div>
+            <div>
+                <label for="reinitialiser">Votre recherche</label>
+                <input type="button" value="Réinitialiser" id="reinitialiser">
+            </div>
         </fieldset>
+        <fieldset>
+            <legend>Bouton envoi</legend>
+            <input type="submit" value="Envoyer" id="envoyer">
+        </fieldset>
+    </form>
 </body>
 </html>
--- PHP(fonctions)/index.php
+++ PHP(fonctions)/index.php
@@ -20,7 +20,25 @@
         }
     ?>
 
+    <?php
+        function OccCarDansTexte($chaineDepart, $caractere)
+        {
+            $longueurChaine = strlen($chaineDepart);
+            $nbOccurence = 0;
+            for($j = 0; $j < $longueurChaine; $j++)
+            {
+                if($chaineDepart[$j] == $caractere)
+                {
+                    $nbOccurence++;
+                }
+            }
+            return $nbOccurence;
+        }
+    ?>
+
     <!--Partie Affichage-->
     <p>La Factoriellede 7 est <?= Factorielle(7) ?></p>
+
+    <p>Il y a <?= OccCarDansTexte("test", "t") ?> t dans test</p>
 </body>
 </html>
